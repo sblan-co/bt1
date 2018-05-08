e968f66de2a16c2d3505cf9e6e8b662415bb8e64
Sara <mublasa@gmail.com>
2018-05-08 11:57:00 +0200

    Compila

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
 // import { Platform } from 'ionic-angular';
 
 // Firebase
@@ -50,6 +51,7 @@ export const firebaseConfig = {
   ],
   providers: [
     // Platform,
+    Facebook,
     AngularFireAuth,
     StatusBar,
     SplashScreen,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,11 @@ import { NavController, Platform, AlertController } from 'ionic-angular';
 
 // Ionic
 import { StatusBar } from '@ionic-native/status-bar';
+import { Facebook } from '@ionic-native/facebook';
 
 // Firebase
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 // Project Pages
 import { SignUpPage } from '../sign-up/sign-up';
@@ -20,20 +22,22 @@ import { SignUpPage } from '../sign-up/sign-up';
 export class HomePage {
   private plt: Platform;
   private statusBar: StatusBar;
-  
-  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth, public alertCtrl : AlertController) {
+
+  constructor(
+    public navCtrl: NavController,
+    public afAuth: AngularFireAuth,
+    public alertCtrl: AlertController,
+    public fb: Facebook) {
     // this.plt.ready().then ((readySource) =>{
     //   this.statusBar.styleLightContent();
     // });
   }
 
-  signUp()
-  {
+  signUp() {
     this.navCtrl.push(SignUpPage);
   }
 
-  emailLogin(email, password)
-  {
+  emailLogin(email, password) {
     console.log(email);
     console.log(password);
 
@@ -55,8 +59,43 @@ export class HomePage {
           ]
         });
         alert.present();
-    });
-
+      });
   }
-  
+
+  fbLogin(): Promise<any> {
+    return this.fb.login(['public_profile', 'user_birthday', 'email']).then(
+      async (res) => {
+        const firecreds = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+        const FBProfile = await this.fb.api('me?fields=email,first_name,last_name,user_birthday', []);
+
+        let firstName: string = FBProfile['first_name'];
+        let surname: string = FBProfile['last_name'];
+        let email: string = FBProfile['email'];
+        let userBirthday: string = FBProfile['user_birthday'];
+
+        try {
+          const success = await firebase.auth().signInWithCredential(firecreds).then(
+            user => {
+              let userRef = firebase.database().ref('/users/' + user.uid);
+
+              userRef.update({
+                'email': email,
+                'created': firebase.database.ServerValue.TIMESTAMP,
+                'firstname': firstName,
+                'surname': surname,
+                'user_birthday': userBirthday
+              });
+            }
+          )
+          //this.navCtrl.push(TabsPage);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    ).catch(
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
